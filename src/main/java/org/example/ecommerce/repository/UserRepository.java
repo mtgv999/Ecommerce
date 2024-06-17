3aca905d06430943de76179ba105e18505a3a47b
mtgv999 <petermgcz@gmail.com>
2024-06-17 09:38:11 +0900

    Commit14 - Customer, Seller, Product, Cart, Orders Controller를 Request 요청으로 바꿈, Customer, Seller에 로그인, 로그아웃, getProfile 메서드추가

--- src/main/java/org/example/ecommerce/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-//package org.example.ecommerce.repository;
-//
-//import org.example.ecommerce.domain.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User,Long> {}
